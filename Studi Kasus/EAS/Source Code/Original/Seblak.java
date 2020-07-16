04a82e9d6f2ed3e66518552f6ba50a4b8025fd80
ReyRizki <reynaldirizki97.rr@gmail.com>
2020-07-16 10:22:59 +0700

    init original folder

---
+++ Studi Kasus/EAS/Source Code/Original/Seblak.java
@@ -0,0 +1,11 @@
+package Original;
+
+public class Seblak {
+    public float harga;
+    public String nama;
+
+    public Seblak(String nama, float harga) {
+        this.nama = nama;
+        this.harga = harga;
+    }
+}
